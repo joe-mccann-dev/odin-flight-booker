5c5b8bae98c789e9ee8c625c8d139028a04da27c
Tenacious-Qi <joe.mccann@mailbox.org>
2021-05-27 17:05:15 -0400

    move airports instance variable to class method. move flights to its own method.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,21 +1,29 @@
 class FlightsController < ApplicationController
 
   def index
-    @airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
-    @all_flights = Flight.all.order(:departure_time)
-    @departure_times = @all_flights
+    # @airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
+    # @all_flights = Flight.all.order(:departure_time)
+    @departure_times = flights
                        .map { |flight| [ flight.departure_time_formatted, flight.departure_time ] }
                        .uniq { |formatted_time, full_time| formatted_time }
-    @flights_to_display_at_index = @all_flights.limit(20)
-    @flights =  if params[:origin_id].nil?
-                  @flights_to_display_at_index
-                else
-                  date = params[:departure_time].to_datetime
-                  @all_flights.where(origin_id: params[:origin_id],
-                               destination_id: params[:destination_id],
-                               departure_time: flights_on_selected_date(date) )
-                end
+    @flights_to_display_at_index = flights.limit(20)
+    @available_flights = find_available_flights
     
+  end
+
+  def flights
+    Flight.all.order(:departure_time)
+  end
+
+  def find_available_flights
+    if params[:origin_id].nil?
+      @flights_to_display_at_index
+    else
+      date = params[:departure_time].to_datetime
+      flights.where(origin_id: params[:origin_id],
+                   destination_id: params[:destination_id],
+                   departure_time: flights_on_selected_date(date) )
+    end
   end
 
   def flights_on_selected_date(date)
--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,9 @@
 class Airport < ApplicationRecord
   has_many :departing_flights, class_name: 'Flight', foreign_key: 'origin_id'
   has_many :arriving_flights,  class_name: 'Flight', foreign_key: 'destination_id'
+
+  def self.options_for_select
+    # all.map { |airport| [ airport.code, airport.id ] }
+    all.pluck(:code, :id)
+  end
 end
