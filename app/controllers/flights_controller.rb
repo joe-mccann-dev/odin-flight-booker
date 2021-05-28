c6641dbcaac89421f6dc6e841ce6922ceb1ded30
Tenacious-Qi <joe.mccann@mailbox.org>
2021-05-28 00:11:20 -0400

    Refactor FlightsController.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,14 +1,7 @@
 class FlightsController < ApplicationController
 
   def index
-    # @airports = Airport.all.map { |airport| [ airport.code, airport.id ] }
-    # @all_flights = Flight.all.order(:departure_time)
-    @departure_times = flights
-                       .map { |flight| [ flight.departure_time_formatted, flight.departure_time ] }
-                       .uniq { |formatted_time, full_time| formatted_time }
-    @flights_to_display_at_index = flights.limit(20)
     @available_flights = find_available_flights
-    
   end
 
   def flights
@@ -16,22 +9,15 @@ class FlightsController < ApplicationController
   end
 
   def find_available_flights
-    if params[:origin_id].nil?
-      @flights_to_display_at_index
-    else
-      date = params[:departure_time].to_datetime
-      flights.where(origin_id: params[:origin_id],
-                   destination_id: params[:destination_id],
-                   departure_time: flights_on_selected_date(date) )
-    end
+    return flights.limit(20) unless params[:origin_id].present?
+    
+    date = params[:departure_time].to_datetime
+    flights.where(origin_id: params[:origin_id],
+                  destination_id: params[:destination_id],
+                  departure_time: within_selected_date(date) )
   end
 
-  def flights_on_selected_date(date)
-    DateTime.new(date.year, date.month, date.day, 0, 0, 0)..
-    DateTime.new(date.year, date.month, date.day, 23, 59, 59)
-  end
-
-  def flight_params
-    params.require(:flight).permit(:origin_id, :destination_id, :departure_time)
+  def within_selected_date(date)
+    DateTime.new(date.year, date.month, date.day).all_day
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,10 +2,9 @@ class Flight < ApplicationRecord
   belongs_to :origin_airport,      class_name: 'Airport', foreign_key: 'origin_id'
   belongs_to :destination_airport, class_name: 'Airport', foreign_key: 'destination_id'
 
-  scope :current_flights, -> { where("departure_time > ?", Time.zone.today) }
+  # scope :current_flights, -> { where("departure_time > ?", Time.zone.today) }
 
-  def departure_time_formatted
-    departure_time.strftime("%m/%d/%Y")
+  def self.options_for_select
+    all.order(:departure_time).pluck(:departure_time).map(&:to_date).uniq
   end
-
 end
