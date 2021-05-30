54c600894191b8e91d314cb3c8fa6e5d5e656e86
Tenacious-Qi <joe.mccann@mailbox.org>
2021-05-30 00:34:54 -0400

    Setup relationship between a flight and its passengers.

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,6 @@
 class Flight < ApplicationRecord
   has_many :bookings
+  has_many :passengers, through: :bookings
   belongs_to :origin_airport,      class_name: 'Airport', foreign_key: 'origin_id'
   belongs_to :destination_airport, class_name: 'Airport', foreign_key: 'destination_id'
 
