7c4c3e664b41eb5c67661529163b5cb5ce930fb2
Tenacious-Qi <joe.mccann@mailbox.org>
2021-05-31 01:06:29 -0400

    Remove unecessary column from Bookings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_30_042355) do
+ActiveRecord::Schema.define(version: 2021_05_31_050348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2021_05_30_042355) do
     t.integer "passenger_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "tickets"
   end
 
   create_table "flights", force: :cascade do |t|
