dd30d34abe92dce84db534f32f957e5e213db9a6
Tenacious-Qi <joe.mccann@mailbox.org>
2021-08-26 01:13:19 -0400

    Use #email_address_with_name.

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -5,6 +5,9 @@ class PassengerMailer < ApplicationMailer
     @booking = params[:booking]
     @passenger = params[:passenger]
     @booking_url = "http://127.0.0.1:3000/bookings/#{@booking.id}"
-    mail(to: @passenger.email, subject: 'Booking Confirmation')
+    mail(
+      to: email_address_with_name(@passenger.email, @passenger.name),
+      subject: 'Booking Confirmation'
+    )
   end
 end
