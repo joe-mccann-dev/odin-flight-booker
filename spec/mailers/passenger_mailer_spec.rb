db86d86e96cb1a494b8c45f75644254aeb4336d4
Tenacious-Qi <joe.mccann@mailbox.org>
2021-08-26 17:39:44 -0400

    Add url example to PassengerMailer spec.

--- spec/mailers/passenger_mailer_spec.rb
+++ spec/mailers/passenger_mailer_spec.rb
@@ -5,11 +5,16 @@ RSpec.describe PassengerMailer, type: :mailer do
     let(:booking) { create(:booking) }
     let(:passenger) { create(:passenger) }
     let(:mail) { PassengerMailer.with(booking: booking, passenger: passenger).confirmation_email }
+    let(:url) { "http://localhost:3000/bookings/#{booking.id}" }
 
     it "renders the headers" do
       expect(mail.subject).to eq("Booking Confirmation")
       expect(mail.to).to eq([passenger.email])
       expect(mail.from).to eq(["joe.mccann.dev@gmail.com"])
+    end
+
+    it "includes a url to the booking page" do
+      expect(mail.body.encoded).to include(url)
     end
   end
 end
