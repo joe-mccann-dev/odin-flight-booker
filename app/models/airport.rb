756ae6c7baf9bcebac3f26d6bce496ab923865cb
Tenacious-Qi <joe.mccann@mailbox.org>
2021-09-19 02:30:01 -0400

    Make inverse_of explicit. Add comments to Airport and Flight.

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -2,8 +2,19 @@
 
 # limited number of airports. Can be selected at '/flights'
 class Airport < ApplicationRecord
-  has_many :departing_flights, class_name: 'Flight', foreign_key: 'origin_id'
-  has_many :arriving_flights,  class_name: 'Flight', foreign_key: 'destination_id'
+  # how to identify an origin_airport's departing_flights
+  # will look for column flights.airport_id unless foreign_key is specified...
+  # EXAMPLE Airport.last.departing_flights.last.origin_airport will not work, because 
+  # ActiveRecord will try to find flights.airport_id which there is no column for and is not specific enough--
+  # the origin_airport is necessary information for this use case.
+  # ActiveRecord needs to know each departing_flight contains a reference to its origin_airport (the foreign_key: origin_id)
+  has_many :departing_flights, class_name: 'Flight',
+                               foreign_key: 'origin_id',
+                               inverse_of: :origin_airport
+
+  has_many :arriving_flights,  class_name: 'Flight',
+                               foreign_key: 'destination_id',
+                               inverse_of: :destination_airport
 
   def self.options_for_select
     all.pluck(:location, :id)
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -4,8 +4,18 @@
 class Flight < ApplicationRecord
   has_many :bookings, dependent: :destroy # there can't be a booking without a flight
   has_many :passengers, through: :bookings
-  belongs_to :origin_airport,      class_name: 'Airport', foreign_key: 'origin_id'
-  belongs_to :destination_airport, class_name: 'Airport', foreign_key: 'destination_id'
+  # how to identify a departing_flight's origin_airport
+  # A departing_flight needs to know its origin_airport is accessed via the origin_id key
+  # An origin_airport 'owns' all departing_flights and references itself as 'origin_id'
+  belongs_to :origin_airport,      class_name: 'Airport',
+                                   foreign_key: 'origin_id',
+                                   inverse_of: :departing_flights
+  # how to identify an arriving_flight's destination_airport
+  # An arriving_flight needs to know its destination_airport is accessed via the destination_id key                                   
+  # A destination_airport 'owns' all arriving_flights and references itself as 'destination_id'
+  belongs_to :destination_airport, class_name: 'Airport',
+                                   foreign_key: 'destination_id',
+                                   inverse_of: :arriving_flights
 
   def self.options_for_select
     all.order(:departure_time).pluck(:departure_time).map(&:to_date).uniq
